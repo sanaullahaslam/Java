ce7459b87aa0b53d06a69af232e2ffb538b4e45b
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-04 09:21:11 -0700

    Deleted all Print statements

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,7 @@ class HelloWorld {
 
 public static void main (String args[]) {
 
-
-System.out.println("Hello World"); 
-System.out.println("Hello Programmer"); 
-System.out.println("Hello Computer Science Student");
-System.out.println("Hello Java"); 
+System.out.println(" ");
 
      
                                         }
