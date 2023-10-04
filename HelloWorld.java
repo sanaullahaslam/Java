e1a1c1ef4ac63b2830addfc83640d2ced20c7be4
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-04 09:22:20 -0700

    Hello world & Hello Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ class HelloWorld {
 
 public static void main (String args[]) {
 
-System.out.println(" ");
+System.out.println("Hello World");
+System.out.println("Hello Java");
 
      
                                         }
