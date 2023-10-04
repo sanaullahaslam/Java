00b4176dfc2d3f3cb356112a055dbc394d1e79ca
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-04 09:18:53 -0700

    New Line Hello Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,9 @@ public static void main (String args[]) {
 
 System.out.println("Hello World"); 
 System.out.println("Hello Programmer"); 
-System.out.println("Hello Computer Science Student"); 
+System.out.println("Hello Computer Science Student");
+System.out.println("Hello Java"); 
+
      
                                         }
 
