5845005ee6e236afb8fd10507bd95e29cc67462d
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-05 04:52:54 -0700

    String added

--- Variables.java
+++ Variables.java
@@ -6,8 +6,10 @@ public static void main (String args[]) {
 int num=10;
 float val=4.5f;
 bool y=true;
+String name="Programmer";
 
 
+System.out.println("String variable = "+name);
 System.out.println("Int num  = "+num);
 System.out.println("Float values = "+val);
 System.out.println("Boolean = "+y);
