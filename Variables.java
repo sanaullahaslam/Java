f50eb006ce8dedede6a01a095c714505e3086b4a
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-05 04:48:14 -0700

    Variable program

---
+++ Variables.java
@@ -0,0 +1,13 @@
+class Variables{
+
+
+public static void main (String args[]) {
+
+int num=10;
+
+System.out.println("Int num  = "+num);
+
+     
+                                        }
+
+                 } 
