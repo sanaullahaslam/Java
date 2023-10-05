32a85b151065e0336bd833f9620e9335493541f3
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-05 04:51:44 -0700

    Boolean added

--- Variables.java
+++ Variables.java
@@ -5,10 +5,12 @@ public static void main (String args[]) {
 
 int num=10;
 float val=4.5f;
+bool y=true;
 
 
 System.out.println("Int num  = "+num);
 System.out.println("Float values = "+val);
+System.out.println("Boolean = "+y);
 
      
                                         }
