34d48569293d3216b41749010dd0443c371b7de7
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-05 04:50:53 -0700

    Float variable added

--- Variables.java
+++ Variables.java
@@ -4,8 +4,11 @@ class Variables{
 public static void main (String args[]) {
 
 int num=10;
+float val=4.5f;
+
 
 System.out.println("Int num  = "+num);
+System.out.println("Float values = "+val);
 
      
                                         }
