cfde1631f51a0d771893408454e008544b472d8f
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-10-09 22:50:51 -0700

    new program

---
+++ sample.java
@@ -0,0 +1,14 @@
+class sample{
+
+
+public static void main (String args[]) {
+
+String name="Sanaullah";
+
+
+System.out.println("Hello "+name);
+
+     
+                                        }
+
+                 } 
